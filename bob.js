2f939efd5e258df872a3ce1fb97331a0533d9dc2
Daniel Stark <daniel@studiostark.com>
2013-06-17 13:35:02 -0700

    bob in javascript

---
+++ bob.js
@@ -0,0 +1,13 @@
+var bob = function(input) {
+    if(input.charAt(input.length - 1) === "?") {
+    console.log("Sure.");
+    } else if(input.charAt(input.length - 1) === ".") {
+    console.log("Whatever.");
+    } else if(input === input.toUpperCase()) {
+    console.log("Woah, chill out!");
+    } else if(input === "") {
+    console.log("Fine. Be that way!");
+    }
+}
+
+bob("YELL!")
