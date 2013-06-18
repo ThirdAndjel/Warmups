09ab02985a6334869a227592b8cfe24ec41eb868
Daniel Stark <daniel@studiostark.com>
2013-06-18 13:48:32 -0700

    gah, js

--- game/game.js
+++ game/game.js
@@ -1,3 +1,32 @@
+// $(document).ready(function() {
+// 	$('.left').on('click', function() {
+// 		if ($(this).css('background-color') === 'rgb(211, 211, 211)') {
+// 			$(this).css('background-color', 'rgb(0, 0, 255)');
+// 		} else {
+// 			$(this).css('background-color', 'rgb(211, 211, 211)');
+// 		}
+// 	} );
+// });
+
+// $(document).ready(function() {
+// 	$('.left').on('click', function() {
+// 		if ($('.left').filter(function() {
+// 			if ($(this).css('background-color') === 'rgb(0, 0, 255)') {
+// 				return true;
+// 			}
+// 			return false;
+// 		}).length === 0) {
+// 			$('.hidden').removeClass('hidden');
+// 		}
+// 	});
+// });
+
+// $(document).ready(function() {
+// 	$('a').on('click', function() {
+// 		$('.left').css('background-color', '');
+// 	});
+// });
+
 $(document).ready(function() {
 	$('.left').on('click', function() {
 		if ($(this).css('background-color') === 'rgb(211, 211, 211)') {
@@ -6,19 +35,20 @@ $(document).ready(function() {
 			$(this).css('background-color', 'rgb(211, 211, 211)');
 		}
 	} );
-});
 
-$(document).ready(function() {
+	$('.left').on('click', function() {
+		if ($('.left').filter(function() {
+			if ($(this).css('background-color') === 'rgb(0, 0, 255)') {
+				return true;
+			}
+				return false;
+			}).length === 0) {
+			$('.hidden').removeClass('hidden');
+		}
+	});
+
 	$('a').on('click', function() {
 		$('.left').css('background-color', '');
 	});
 });
 
-// $(document).ready(function() {
-// 	if ($('.left').css('background-color') === 'rgb(0, 0, 255)') {
-// 		$('.hidden').removeClass('.hidden');
-// 	}
-// });
-
-
-if ($('.left').css('background-color') === 'rgb(0, 0, 255)') {$('.hidden').css('display','block');}
