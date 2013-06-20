b3941ea6f6466e5275bcf5514479f1dfdc59ac0f
Daniel Stark <daniel@studiostark.com>
2013-06-19 17:29:51 -0700

    progress

--- 5boxgame/game_table.js
+++ 5boxgame/game_table.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//THESE VARS NEED TO BE SET OUTSIDE OF CLICK HANDLER
 	var turn = true;
 	var xhigh = 5;
 	var xlow = 1;
@@ -19,27 +20,38 @@ $(document).ready(function(){
 // if not
 // count rows
 // add tds to each existing row
-/////////////////////In here expansion logic/////////////////////////////////
-	// $("table").on('click',"td",function() {
-	// 	var xcord = $(this).data('row');
-	// 	var ycord = $(this).data('column');
+	//CLICK HANDLER
+	$("table").on('click',"td",function() {
+		//CAPTURE X,Y COORDINATE FROM CELL CLICKED ON.
+		var xcord = $(this).data('row');
+		var ycord = $(this).data('column');
 
-	// 	if((xcord + 1) > xhigh) {
-	// 		xhigh += 1;
-	// 		$('tr[data-row="' + xcord + '"]').clone(false)
-	// 	}
-	// 	if((xcord - 1) < xlow) {
-	// 		xlow -= 1;
-	// 	}
-	// 	if((ycord + 1) > yhigh) {
-	// 		yhigh += 1;
-	// 	}
-	// 	if((ycord -1) < ylow) {
-	// 		ylow -= 1;
-	// 	}
-
-	// 	if(turn === true) {
-//////////////////////////////////////////////////////
+		//HANDLE EXPANDING ROW DOWN
+		if((xcord + 1) > xhigh) {
+			xhigh += 1;
+			$newrow = $('tr[data-row="' + xcord + '"]').clone();
+			($newrow).insertAfter('tr[data-row="' + xcord + '"]');
+			//need to add correct data-row for tr
+			//need to add correct data-row for every td
+			//need to correct xy coordinates in td
+		}
+		//HANDLE EXPANDING ROW UP
+		if((xcord - 1) < xlow) {
+			xlow -= 1;
+			$newrow = $('tr[data-row="' + xcord + '"]').clone(false);
+			($newrow).insertBefore('tr[data-row="' + xcord + '"]');
+		}
+		//HANDLE EXPANDING COLUMNS RIGHT
+		if((ycord + 1) > yhigh) {
+			yhigh += 1;
+		}
+		//HANDLE EXPANDING COLUMNS LEFT
+		if((ycord -1) < ylow) {
+			ylow -= 1;
+		}
+		//IF HANDLES ALTERNATING BETWEEN BLUE AND RED TURNS
+		if(turn === true) {
+			//BLUE TURN
 			$(this).removeClass('red').addClass('blue');
 			$('td[data-row="' + (xcord + 1) + '"][data-column="' + ycord + '"]').removeClass('red').addClass('blue');
 			$('td[data-row="' + (xcord - 1) + '"][data-column="' + ycord + '"]').removeClass('red').addClass('blue');
@@ -48,7 +60,7 @@ $(document).ready(function(){
 			turn = false;
 
 		} else if(turn === false) {
-
+			//RED TURN
 			$(this).removeClass('blue').addClass('red');
 			$('td[data-row="' + (xcord + 1) + '"][data-column="' + ycord + '"]').removeClass('blue').addClass('red');
 			$('td[data-row="' + (xcord - 1) + '"][data-column="' + ycord + '"]').removeClass('blue').addClass('red');
@@ -61,37 +73,3 @@ $(document).ready(function(){
 	});
 
 });
-
-
-
-// version 1
-// $(document).ready(function(){
-// 	var turn = true;
-
-// 	$("table").on('click',"td",function() {
-// 		var xcord = 0;
-// 		var ycord = 0;
-
-// 		if(turn === true) {
-// 			$(this).removeClass('red').addClass('blue');
-// 			xcord = $(this).data('row');
-// 			ycord = $(this).data('column');
-// 			$('td[data-row="' + (xcord + 1) + '"][data-column="' + ycord + '"]').removeClass('red').addClass('blue');
-// 			$('td[data-row="' + (xcord - 1) + '"][data-column="' + ycord + '"]').removeClass('red').addClass('blue');
-// 			$('td[data-column="' + (ycord + 1) + '"][data-row="' + xcord + '"]').removeClass('red').addClass('blue');
-// 			$('td[data-column="' + (ycord - 1) + '"][data-row="' + xcord + '"]').removeClass('red').addClass('blue');
-// 			turn = false;
-// 		} else if(turn === false) {
-// 			$(this).removeClass('blue').addClass('red');
-// 			xcord = $(this).data('row');
-// 			ycord = $(this).data('column');
-// 			$('td[data-row="' + (xcord + 1) + '"][data-column="' + ycord + '"]').removeClass('blue').addClass('red');
-// 			$('td[data-row="' + (xcord - 1) + '"][data-column="' + ycord + '"]').removeClass('blue').addClass('red');
-// 			$('td[data-column="' + (ycord + 1) + '"][data-row="' + xcord + '"]').removeClass('blue').addClass('red');
-// 			$('td[data-column="' + (ycord - 1) + '"][data-row="' + xcord + '"]').removeClass('blue').addClass('red');
-// 			turn = true;
-// 		}
-
-// 	});
-
-// });
